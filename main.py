a18f14995a9d4993f279291d246bf3ee1b2df4b3
Conor Mahany <mahany25@gmail.com>
2023-07-13 18:35:14 +0200

    updated main.py

--- main.py
+++ main.py
@@ -5,41 +5,57 @@ import json
 import os
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def execute_linux_command(command_json):
+def execute_linux_commands(commands_json):
 
-    command_json_d = json.loads(command_json)
-    command = command_json_d['command']
-    args = command_json_d.get('args', [])
+    commands = json.loads(commands_json)
 
-    try:
-        result = subprocess.check_output([command] + args, shell=False)
-        msg = result.decode('utf-8')
-    except subprocess.CalledProcessError as e:
-        msg = f"Error executing command: {e.output.decode('utf-8')}"
-        print(msg)
-    except FileNotFoundError:
-        msg = f"Command not found: {command}"
-        print(msg)
-    except Exception as e:
-        msg = f"An unexpected error occurred: {e}"
-        print(msg)
-    return msg
+    result_str = ''
+    for command_json in commands:
+        command = command_json['command']
+        args = command_json.get('args', [])
+        redirect_out = None
+        
+        if '>' in args:  # detect redirection
+            redirect_index = args.index('>')
+            redirect_out = args[redirect_index + 1]  # get output file 
+            args = args[:redirect_index]  # remove '>' symbols 
 
+        try:
+            if redirect_out:
+                with open(redirect_out, 'w') as fp:
+                    result = subprocess.run([command] + args, stdout=fp)
+                msg = f'REDIRECTED_TO_FILE: {redirect_out}\n' 
+            else:
+                result = subprocess.check_output([command] + args, shell=False)
+                msg = result.decode('utf-8')
+        except subprocess.CalledProcessError as e:
+            msg = f"Error executing command: {e.output.decode('utf-8')}"
+            print(msg)
+        except FileNotFoundError:
+            msg = f"Command not found: {command}"
+            print(msg)
+        except Exception as e:
+            msg = f"An unexpected error occurred: {e}"
+            print(msg)
+
+        result_str += msg + '\n'
+
+    return result_str.strip()
 
 FUNCTIONS = [
     {
-        'name': 'execute_linux_command',
-        'description': 'execute Linux command and return stdout',
+        'name': 'execute_linux_commands',
+        'description': 'execute list of Linux commands and return stdout for each',
         'parameters': {
             'type': 'object',
             'properties': {
-                'command_json': {
+                'commands_json': {
                     'type': 'string',
-                    'description': 'stringified JSON of command to execute. it needs a string attribute `command` and optional attribute `args` is a list of arguments'
+                    'description': 'stringified JSON of list of commands to execute. each element of the list needs a string attribute `command` and optional attribute `args` which is a list of arguments. Redirection should be an argument by itself'
                 },
             }
         },
-        'required': ['command_json']
+        'required': ['commands_json']
     }
 ]
 
